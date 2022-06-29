15f3bc9357e2ec759e3d0dc1a9aa05861a3e2c51
Ajith K.R <46483037+ajithkr12@users.noreply.github.com>
2022-06-29 19:01:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var x, y, z;  // Declare 3 variables
-x = 5;    // Assign the value 5 to x
+x = 555;    // Assign the value 5 to x
 y = 600;    // Assign the value 6 to y
 z = x + y;  // Assign the sum of x and y to z
